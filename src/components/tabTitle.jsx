b98e7bc5f219e97595c2ad33cf9bd00d3062d014
Vinay Raghu <hello@vinayraghu.com>
2020-08-26 16:58:17 -0400

    add click handler

--- src/components/tabTitle.jsx
+++ src/components/tabTitle.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import './tabStyles.scss';
-const TabTitle = ({ title, disabled }) => {
+import React from "react";
+import "./tabStyles.scss";
+const TabTitle = ({ title, disabled, onClick }) => {
   return (
-    <div className={`chewy-tab-title ${disabled ? 'chewy-tab-title--disabled' : ''}`}>
-      { title }
+    <div
+      className={`chewy-tab-title ${
+        disabled ? "chewy-tab-title--disabled" : ""
+      }`}
+      onClick={onClick}
+    >
+      {title}
     </div>
-  )
-}
+  );
+};
 
-export default TabTitle
+export default TabTitle;
--- src/components/tabs.jsx
+++ src/components/tabs.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./tabStyles.scss";
 import TabTitle from "./tabTitle";
 import TabPanel from "./tabPanel";
 
 const Tabs = () => {
+  const [activeTab, setActiveTab] = useState("apple");
+  useEffect(() => {
+    setActiveTab("banana");
+  }, []);
   return (
     <>
       <div className="app-tab-layout">
-        <TabTitle title="apple" disabled />
-        <TabTitle title="banana" />
-        <TabTitle title="cherry" />
+        <TabTitle
+          title="apple"
+          disabled
+          onClick={() => setActiveTab("apple")}
+        />
+        <TabTitle
+          title="banana"
+          onClick={() => {
+            console.log("here");
+            setActiveTab("banana");
+          }}
+        />
+        <TabTitle title="cherry" onClick={() => setActiveTab("cherry")} />
       </div>
       <div>
-        <TabPanel content={"apples are great"} active />
-        <TabPanel content={"bananas are great"} />
-        <TabPanel content={"cherries are great"} />
+        <TabPanel content={"apples are great"} active={activeTab === "apple"} />
+        <TabPanel
+          content={"bananas are great"}
+          active={activeTab === "banana"}
+        />
+        <TabPanel
+          content={"cherries are great"}
+          active={activeTab === "cherry"}
+        />
       </div>
     </>
   );
